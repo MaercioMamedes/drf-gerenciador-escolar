ae583adde1db9fdf80e4c4919b8027ad58fa049b
MaercioMamedes <maerciomamedes@hotmail.com>
2023-06-26 16:56:53 -0300

    implementação da paginção de listas

--- setup/settings.py
+++ setup/settings.py
@@ -145,7 +145,9 @@ REST_FRAMEWORK = {
     #],
     'DEFAULT_AUTHENTICATION_CLASSES':[
         'rest_framework.authentication.BasicAuthentication',
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
 
 CORS_ALLOWED_ORIGINS = [
